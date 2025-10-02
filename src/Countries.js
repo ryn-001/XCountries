f90b2b4f0ee05ceb71870e4ba63dcde313a88efb
Aryan Khaire <aryan.khaire001@gmail.com>
2025-10-02 22:16:40 +0530

    project done

---
+++ src/Countries.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Countries.css";
+
+const Countries = () => {
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        const fetchCountries = async () => {
+            try {
+                const response = await axios.get("https://xcountries-backend.labs.crio.do/all");
+                console.log(response.data);
+                setCountries(response.data);
+            } catch {
+                console.log("An error occured");
+            }
+        }
+
+        fetchCountries();
+    }, [])
+
+    return (
+        <div className="countries">
+            {countries.map((ele) => (
+                <div className="country" key={ele.abbr}>
+                    <img src={ele.flag} alt={ele.name} />
+                    <h3>{ele.name}</h3>
+                </div>
+            )
+            )}
+        </div>
+    )
+}
+
+export default Countries;
